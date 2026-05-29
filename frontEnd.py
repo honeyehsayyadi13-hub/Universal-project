da160990881a388366050d6546e1fb32d974a7d2
Honeyeh Sayyadi <honeyehsayyadi13@gmail.com>
2026-05-28 20:39:25 -0400

    updated project

--- frontEnd.py
+++ frontEnd.py
@@ -1,7 +1,12 @@
 " Program that shows a user an optimized map of getting to rides at the amusement park Universal"
 import pygame
+from tkinter import *
 
 pygame.init()
+win = Tk()
+
+clock = pygame.time.Clock()
+
 
 screen = pygame.display.set_mode((1200,800))
 pygame.display.set_caption("front_end")
@@ -13,6 +18,14 @@ mapImage=pygame.image.load("map.png")
 ##scaling image
 mapImage = pygame.transform.scale(mapImage, (1000, 800))
 
+##clickable buttons work
+
+# button positions
+buttons = [
+    (300, 200, False),
+    (500, 350, False),
+    (700, 150, False)
+]
 
 running = True
 
@@ -22,12 +35,39 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+        if event.type==pygame.MOUSEBUTTONDOWN:
+            mx, my=pygame.mouse.get_pos()
+            for button in buttons:
+                x=button[0]
+                y=button[1]
+                distance=((mx-x)**2+(my-y)**2)*0.5
+                
+                if distance <=10:
+                    button[2]=True
+
+                    
 
     # fill screen
     screen.fill((255,255,255))   
 
     #displaying the image
     screen.blit(mapImage, (0, 0))
+    
+    # draw buttons
+    for button in buttons:
+
+        x = button[0]
+        y = button[1]
+        clicked = button[2]
+
+        # if clicked -> green
+        if clicked:
+            color = (0,255,0)
+        else:
+            color = (255,0,0)
+
+        pygame.draw.circle(screen, color, (x,y), 10)
     
     # update window
     pygame.display.update()
